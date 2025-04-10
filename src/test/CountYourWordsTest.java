6c72aa166629764073b09ae2e610e69ba14f6c29
YashDeCoder <yashisrani35@gmail.com>
2025-04-10 16:17:16 +0200

    Added test cases for the sorting function

--- src/test/CountYourWordsTest.java
+++ src/test/CountYourWordsTest.java
@@ -1,14 +1,48 @@
 package test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import org.junit.Test;
+import main.CountYourWords;
 
 public class CountYourWordsTest {
     // Sorting algorithm
     @Test
-    public void basicTest() {
-        assertEquals(2, 1+1);
+    public void sortEmptyTest() {
+        HashMap<String, Integer> emptyMap = new HashMap<>();
+        ArrayList<String> sortedArray = CountYourWords.sort(emptyMap);
+        assertTrue("Array should be empty", sortedArray.isEmpty());
     }
+    
+    @Test
+    public void sortSingleTest() {
+        HashMap<String, Integer> singleMap = new HashMap<>();
+        singleMap.put("apple", 1);
+        ArrayList<String> sortedArray = CountYourWords.sort(singleMap);
+        ArrayList<String> returnString = new ArrayList<>();
+        returnString.add("apple 1");
+        assertEquals(returnString, sortedArray);
+    }
+    
+    @Test
+    public void sortMultipleTest() {
+        HashMap<String, Integer> multipleMap = new HashMap<>();
+        multipleMap.put("cherry", 1);
+        multipleMap.put("banana", 1);
+        multipleMap.put("apple", 1);
+        ArrayList<String> sortedArray = CountYourWords.sort(multipleMap);
+        
+        List<String> expectedKeys = Arrays.asList("apple 1", "banana 1", "cherry 1");
+        List<String> actualKeys = sortedArray;
 
+        assertEquals("Keys should be sorted", expectedKeys, actualKeys);
+    }
+        
     // Count the words in a list of lines
     
     // Read from file
