17257804012a776af1ad235575eb35fbaeb90a92
YashDeCoder <yashisrani35@gmail.com>
2025-04-11 11:05:41 +0100

    Added the implementation of insertion sort

--- src/main/Sort.java
+++ src/main/Sort.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Sort {
         /* Function to sort array using insertion sort 
@@ -9,7 +10,21 @@ public class Sort {
          * @return ArrayList<String> sortedArray
         */
         public static ArrayList<String> insertionSort(String arr[]) {
-            ArrayList<String> sortedArray = new ArrayList<>();
-            return sortedArray;
+            if (arr == null) throw new IllegalArgumentException();
+            
+            int n = arr.length;
+
+            for (int i = 1; i < n; ++i) {
+                String key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j].compareTo(key) > 0) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+            return new ArrayList<>(Arrays.asList(arr));
         }
 }
